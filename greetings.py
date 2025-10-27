123a18efec5a53c240d1982749b5dce26322b77b
ApongsehIyan23 <apongsehiyan23@gmail.com>
2025-10-27 14:20:43 -0700

    Wrote greetings.py

---
+++ greetings.py
@@ -0,0 +1,9 @@
+def show_intro():
+    print("==============================")
+    print("Welcome to the User Info Program")
+    print("==============================")
+
+
+def show_exit_message():
+    print("Thank you for using the program. Goodbye!")
+
